b5477491a334fd83c56527b943cb3cba1a076479
abel <hack_vip@foxmail.com>
2019-08-22 20:45:13 +0800

    购物车添加商品到本地

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -32,7 +32,24 @@ Page({
         detail:message
       })
     })
-  }
+  },
+  handleAddCart(){
+    wx.showToast({
+      title: '添加购物车成功',
+      icon:'success',
+      duration:2000
+    })
+
+    //获取本地goods列表
+    const goods = wx.getStorageSync("goods")||{}
+
+    //把商品添加到本地的goods对象中
+    const {detail} =this.data
+    goods[detail.goods_id] =detail
+
+    //保存到本地
+    wx.setStorageSync("goods", goods)
+      }
 
 
 })
