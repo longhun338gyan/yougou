2916a5a46b8022ad88bf81ed4fef443ecf535b55
abel <hack_vip@foxmail.com>
2019-08-22 17:30:39 +0800

    搜索内容进行跳转

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,31 +1,47 @@
-// pages/search/index.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    inputValue:'',//输入框的值
+    inputValue: '', //输入框的值
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   //每次输入搜索关键字的时候触发
-  handleInput(ev){
-    const {value} = ev.detail;
+  handleInput(ev) {
+    const {
+      value
+    } = ev.detail;
     this.setData({
-     inputValue:value
+      inputValue: value
+    })
+    //查询搜索建议
+    request({
+      url: "goods/qsearch",
+      data: {
+        query: value
+      }
+    }).then(res => {
+      console.log(res)
     })
   },
   //清空输入的值
-  handleCancel(){
+  handleCancel() {
     this.setData({
-      inputValue:''
+      inputValue: ''
+    })
+  },
+  handleConfirm() {
+    wx.navigateTo({
+      url: '/pages/search_list/index?keyword=' + this.data.inputValue
     })
   }
 })
